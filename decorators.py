0dd8d3961d6fddad941f88be3f1c8b9c07d3c9a8
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-10-07 23:22:10 +0100

    Put the decorators into their own module to keep the notebooks tidy

---
+++ decorators.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+
+def Periodicise(xmin, xmax):
+    """Function decorator that extends a function by making it periodic.
+    """
+    def xshift(x):
+        y = np.remainder(x-xmin, (xmax-xmin)) + xmin
+        return y
+
+    def periodicised(f):
+        return lambda x: f(xshift(x))
+
+    return periodicised
+
+
+def Reflect(sig, about=0):
+    """Function decorator that extends a function by reflecting it."""
+    if sig not in (1, -1, "even", "odd"):
+        raise ValueError
+    if sig == "even": sig = 1
+    if sig == "odd": sig = -1
+
+    def inner(f, x):
+        if x > about:
+            return f(x)
+        elif x == about:
+            return f(x) if sig == 1 else 0
+        else:
+            return sig * f(2*about - x)
+
+    def reflected(f):
+        return lambda x: inner(f, x)
+
+    return reflected
+
+
+def Clipping(maxamp=1):
+    """Function decorator that returns a clipped version of a function.
+    """
+    def inner(f, x):
+        y = f(x)
+        return max(-1, min(1, y))
+
+    def clipped(f):
+        return lambda x: inner(f, x)
+
+    return lambda f: clipped(f)
+
+
+def CompactSupport(xmin, xmax):
+    """Function decorator that returns a version of the function that is
+    zero outside of specified interval.
+    """
+    def compact_f(f):
+        return lambda x: 0 if (x > xmax or x < xmin) else f(x)
+
+    return compact_f
+
+
+def Vectorize(f):
+    """Wrapper around np.vectorize for floats."""
+    return np.vectorize(f, otypes=[float])
